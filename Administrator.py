8b401414c5e9ed6658b55cbb330fb7d1a69fb34c
cloudsoswift <magic9282m@gmail.com>
2021-05-04 03:14:44 +0900

    Revert "update admin"
    This reverts commit 44129a6e

--- Administrator.py
+++ Administrator.py
@@ -2,17 +2,14 @@ from Music_Tree_Server import db_connector
 
 
 def song_modify(input):
-    # input = [ 수정할 특성명, 넣을 데이터, 수정할 노래 id ]
+    # input = [ '가사', '관련성', '분위기', 수정할 노래 id ]
     db = db_connector.DbConnector()
     db.connect()
     try:
         with db.connection.cursor() as cursor:
-            sql = "update music_tree.song set {} = %s where id=%s;"
-            sql = sql.format(input[0])  # 수정할 특성 명을 {}부분에 넣어줌
-            del input[0]
-            result = cursor.execute(sql, input)
+            sql = "update music_tree.song set lyrics=%s,relevance=%s,mood=%s where songid=%s;"
+            cursor.execute(sql, input)
             db.connection.commit()
-            return result
     finally:
         db.close()
 
@@ -23,25 +20,23 @@ def song_delete(input):
     db.connect()
     try:
         with db.connection.cursor() as cursor:
-            sql = "delete from music_tree.song where id='%s';"
-            result = cursor.execute(sql, input)
+            sql = "delete from music_tree.song where songid='%s';"
+            cursor.execute(sql, input)
             db.connection.commit()
-            return result
     finally:
         db.close()
 
 
 def song_inquiry(input):
-    # input = 조회할 노래 시작점 ( 배열 인덱스 ) / 시작점부터 50곡 id 오름차순 정렬해 불러옴
+    # input = 조회할 노래 시작점 ( 배열 인덱스 ) / 시작점부터 50곡 불러옴
     db = db_connector.DbConnector()
     db.connect()
     try:
         with db.connection.cursor() as cursor:
-            sql = "SELECT * FROM music_tree.song order by id limit %s, 50;"
-            cursor.execute(sql, input-1)
+            sql = "SELECT * FROM music_tree.song limit %s, 50;"
+            cursor.execute(sql, input)
             result = cursor.fetchall()
             return result
     finally:
         db.close()
-
 
--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template, request, session, jsonify
 from flask_socketio import SocketIO, send, emit, join_room,leave_room
 from flask_cors import CORS
 from db_connector import DbConnector
-from Music_Tree_Server import Administrator
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
@@ -25,11 +24,11 @@ def main():
     return render_template("index.html")
 
 # 접속하는 url
-@app.route('/song-info', methods=['GET'])
+@app.route('/song-info', methods=["GET"])
 def song():
+
     # db 조회
-    index = request.args.get('index')  # /song-info?index=
-    result = Administrator.song_inquiry(index)
+
     # test 더미데이터
     data = [
         {
@@ -129,37 +128,8 @@ def song():
             "words": ""
         }
     ]
-    print(result)
-    return jsonify(result)
-
-
-#관리자 노래 수정
-@app.route('/admin/modify', methods=['POST'])
-def admin_modify():
-    input = request.get_json()
-    # input = {"id" : {"type_name" : {"value" : "?"}}}
-    id = list(input)[0]
-    type_name = list(input.get(id))[0]
-    value = list(input.get(id).get(type_name))[0]
-    status = Administrator.song_modify([type_name, value, id])  # 수행 결과. 수정된 row 갯수 반환.
-    if status == 1:
-        status = "yes"
-    else:
-        status = "no"
-    return jsonify({"result": status})
-
-
-#관리자 노래 삭제
-@app.route('/admin/delete', methods=['POST'])
-def admin_delete():
-    input = request.get_json()
-    id = input.get("id")
-    status = Administrator.song_delete(id)
-    if status == 1:
-        status = "yes"
-    else:
-        status = "no"
-    return jsonify({"result": status})
+    print(data)
+    return jsonify(data)
 
 ## 소켓 #####################################################
 
@@ -176,7 +146,7 @@ def on_join(data):
     emit("response", question.firstQuestion(session['socketId']), to=socketId)
 
 @socketIo.on("answer",namespace='/prediction')
-def requestl(ans):
+def request(ans):
     print("ans: ",ans)
     # 데이터 보내는 함수 생성
     ## 프로토콜 type 1: 일반질문, 2: 가사 ,3: 결과
@@ -230,7 +200,7 @@ def requestl(ans):
 
 #https://flask-socketio.readthedocs.io/en/latest/
 if __name__=="__main__":
-	socketIo.run(app,host='0.0.0.0',port=5000)
+	socketIo.run(app)
 
   # app.run(debug=True)
   # host 등을 직접 지정하고 싶다면
