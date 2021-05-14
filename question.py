c593b32320a7a3c9e4e3ed3a3b2b1a6bb76ebc92
root <km78555@gmail.com>
2021-05-14 12:21:23 +0900

    add filter_song_list function

--- question.py
+++ question.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 
-def firstQuestion(socketId):
-    data = {
-                "step": "1",
-                "q":"1번 질문입니다.",
-                'socketId': socketId
-            }
-    return data
+# song_list를 사용자의 입력에 맞게 필터링
+# 예) 클라이언트로부터 성별이 남성이라는 입력이 들어왔을 때,
+# attribute : gender, value : 1(남성)으로 함수 인자 설정
+def filter_song_list(song_list, attribute, value):
+	result = []
+	for i in song_list:
+		if i[attribute] == value:
+			result.append(i)
+	return result
+
+
+def create_question(socket_id):
+	data = {
+		"step" : "1",
+		"q" : "1번 질문입니다.",
+		"socketId" : socket_id
+	}
+	return data
+
+
+if __name__ == '__main__':
+	song_list = [{'song_id' : 100, 'gender' : 1, 'type' : 1}, {'song_id' : 123, 'gender' : 2, 'type' : 1}]
+	print(filter_song_list(song_list, 'type', 1))
