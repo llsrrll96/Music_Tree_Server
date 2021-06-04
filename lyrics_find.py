f7b3b224db81042e98334e6f4b3b90c36c1d8db9
km7865 <rjsxo789@naver.com>
2021-06-04 09:14:17 +0900

    modify lyrics_find logic

--- lyrics_find.py
+++ lyrics_find.py
@@ -17,30 +17,32 @@ class LyricsFind:
 
     def compare_lyrics(self, lyrics):
         for l in self.lyrics_list:
-            print(l['lyrics'])
             if l['lyrics'].find(lyrics) != -1:
-                return True
+                print(l['song_id'], l['title'], '노래와 매칭됨')
+                return l['song_id']
 
-        return False
+        return 0
 
     def cosine_similarity(self, a, b):
         return dot(a, b) / (norm(a) * norm(b))
 
     # 노래 가사와 입력된 가사의 코사인 유사도 측정 후 유사도 반환
-    def measure_similarity(self, lyrics, lyrics_words):
+    def measure_similarity(self, input_words, lyrics_words):
         hannanum = Hannanum()
-        input_words = hannanum.nouns(lyrics)
         vector1 = np.ones(len(lyrics_words))
         vector2 = np.zeros(len(lyrics_words))
         is_zero = True
 
+        print("가사 배열: ", lyrics_words)
+        print("매칭된 단어: ", end='')
         for i in input_words:
             for j in range(len(lyrics_words)):
                 if i == lyrics_words[j]:
                     vector2[j] = 1
+                    print(lyrics_words[j], end=' ')
                     is_zero = False
                     break
-
+        print()
         if is_zero:
             return 0.0
 
@@ -48,18 +50,25 @@ class LyricsFind:
 
     # 노래 가사 중 유사도가 가장 높은 노래의 song_id 반환
     def max_similarity(self):
-        l = self.lyrics_input
+        konlpy = Hannanum()
+        l = konlpy.nouns(self.lyrics_input)
         lyrics_list = self.lyrics_list
-        hannanum = Hannanum()
-        song_id = 1
+        song_id = 0
         max_similarity = 0.0
 
+        result = self.compare_lyrics(self.lyrics_input)
+        if result > 0:
+            return result
+
+        print("입력된 가사의 단어 배열: ", l)
         for lyrics in lyrics_list:
             if lyrics['words'] is None:
-                lyrics['words'] = hannanum.nouns(lyrics['lyrics'])
+                lyrics['words'] = konlpy.nouns(lyrics['lyrics'])
 
+            print("song_id, title: ", lyrics['song_id'], lyrics['title'])
             temp = self.measure_similarity(l, lyrics['words'])
-
+            print("코사인 유사도: ", temp)
+            print()
             if temp > max_similarity:
                 song_id = lyrics['song_id']
                 max_similarity = temp
@@ -69,13 +78,15 @@ class LyricsFind:
 
 if __name__ == '__main__':
     # test
-    lyrics = '밤하늘의 별을 따서 너에게 줄래 너는 내가 사랑하니까 더 소중하니까 오직 너 아니면 안 된다고 외치고 싶어 그저 내 곁에만 있어줘 떠나지 말아줘 참 많이 어색했었죠 널 처음 만난 날 ' \
-             '멀리서 좋아하다가 들킨 사람처럼 숨이 가득 차올라서 아무 말 하지 못했는데 너는 말 없이 웃으며 내 손 잡아줬죠 '
-    lf = LyricsFind('밤하늘에 별을',
-                    [{'words': None, 'song_id': 1, 'lyrics': lyrics}, {'words': None, 'song_id': 124, 'lyrics': lyrics},
-                     {'words': None, 'song_id': 125, 'lyrics': lyrics}])
+    # 원 가사: 밤하늘의 별을 따서 너에게 줄래
+    lyrics_input = '밤하늘의 별을 따서 너에게 줄래'
+    db = DbConnector()
+    song_list = db.select_ballad(10)
+    for i in range(len(song_list)):
+        song_list[i]['words'] = song_list[i]['words'].split()
+
+    lf = LyricsFind(lyrics_input, song_list)
     start = time.time()
-    print('start: ', start)
+    print('lyrics input: ', lyrics_input)
     print(lf.max_similarity())
-    print(lf.compare_lyrics('a'))
-    print('end: ', (time.time() - start))
+    print('lyrics find time (10 songs):', round(time.time() - start, 4), 'sec')
